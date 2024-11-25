a241951f8347cdb7925a14bc5f59029944c64155
HafflePaffle <Hafsteinn.pe@gmail.com>
2024-11-25 16:08:54 +0100

    a

--- VR Miniprojekt/Assets/Scripts/StaminaManager.cs
+++ VR Miniprojekt/Assets/Scripts/StaminaManager.cs
@@ -1,55 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class StaminaManager : MonoBehaviour
 {
     public float maxStamina = 100;
     public float currentStamina;
     private bool gripping;
+    private bool depleted = false;
     [SerializeField] private float staminaLoss;
     [SerializeField] private float staminaRecover;
+    [SerializeField] private float cooldown = 1;
+    private XRDirectInteractor interactor;
 
     void Start()
     {
         currentStamina = maxStamina;
+        interactor = GetComponent<XRDirectInteractor>();
     }
 
     void LateUpdate()
     {
-        if(gripping)
+        if(!depleted)
         {
-            bool canGrip = currentStamina > 0;
-            if (canGrip)
+            if (gripping)
             {
-                currentStamina -= staminaLoss;
+                bool canGrip = currentStamina > 0;
+                if (canGrip)
+                {
+                    currentStamina -= staminaLoss * Time.deltaTime;
+                }
+                else
+                {
+                    currentStamina = 0;
+                    ReleaseGrip();
+                }
             }
             else
             {
-                currentStamina = 0;
-                ReleaseGrip();
+                if (currentStamina < maxStamina)
+                {
+                    currentStamina += staminaRecover * Time.deltaTime;
+                }
+                else
+                {
+                    currentStamina = maxStamina;
+                }
             }
         }
-        else
-        {
-            if (currentStamina < maxStamina)
-            {
-                currentStamina += staminaRecover;
-            }
-            else
-            {
-                currentStamina = maxStamina;
-            }
-        }
+        
     }
 
     private void ReleaseGrip()
     {
         gripping = false;
+        depleted = true;
+        Physics.IgnoreLayerCollision(8,6, true);
+        interactor.enabled = false;
+        StartCoroutine(Cooldown());
+
     }
 
     public void Grip()
     {
         gripping = true;
+    }
+
+    public void Ungrip()
+    {
+        gripping = false;
+    }
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(cooldown);
+        depleted = false;
+        Physics.IgnoreLayerCollision(8, 6, false);
+        interactor.enabled = true;
     }
 }
