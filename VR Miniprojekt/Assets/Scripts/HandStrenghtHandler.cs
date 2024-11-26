7705bb38207be4177e9ab08f17fb19558f91e7a2
HafflePaffle <Hafsteinn.pe@gmail.com>
2024-11-26 11:29:28 +0100

    a

--- VR Miniprojekt/Assets/Scripts/HandStrenghtHandler.cs
+++ VR Miniprojekt/Assets/Scripts/HandStrenghtHandler.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class HandStrenghtHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 prevPosition;
+    private Rigidbody rb;
+    public Transform handTarget;
+    private void Start()
     {
-        
+        prevPosition = transform.position;
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector3 handVel = (handTarget.localPosition - prevPosition) / Time.fixedDeltaTime;
+        float drag = 1/handVel.magnitude + 0.01f;
+
+        drag = drag >= 1 ? 1 : drag;
+        drag = drag <= 0.3f ? 0.3f : drag;
+        rb.AddForce(-rb.velocity * Mathf.Pow(drag, 2));
+
+        prevPosition = transform.position;
     }
 }
